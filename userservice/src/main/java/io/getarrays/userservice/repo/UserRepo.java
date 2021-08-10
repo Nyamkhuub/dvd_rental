54521e1140446a20a74cf8d984d9c57280b2ac2a
Jimmy Columbus <70878088+JiCol@users.noreply.github.com>
2021-08-10 22:00:07 +0800

    Delete userservice directory

--- userservice/src/main/java/io/getarrays/userservice/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package io.getarrays.userservice.repo;
-
-import io.getarrays.userservice.domain.User1;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User1, Long> {
-    User1 findByUsername(String username);
-
-}
